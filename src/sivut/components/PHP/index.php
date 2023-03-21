993419ee7e9c7384250c6f9b80f37102fde1b9d1
Rafael_Cakici <n2cara00@students.oamk.fi>
2023-03-21 17:21:28 +0200

    PHP ja MYSQL jotain tuotteita

---
+++ src/sivut/components/PHP/index.php
@@ -0,0 +1,10 @@
+<?php
+require "dbconnection.php";
+$dbcon = createDbConnection();
+
+
+//$sql = "CREATE TABLE product(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL, PRIMARY KEY(id))";
+
+$sql = "INSERT INTO product (name) VALUES ('Tuutti'),('Strösseli'),('Jaatelokone'),('Kastike')";
+
+$dbcon ->exec($sql);
