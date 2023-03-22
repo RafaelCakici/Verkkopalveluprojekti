fadff2e3a15e41df485c09ac542b9cdc77212442
Rafael_Cakici <n2cara00@students.oamk.fi>
2023-03-22 16:15:48 +0200

    SQL ja PHP

--- src/sivut/components/PHP/index.php
+++ src/sivut/components/PHP/index.php
@@ -3,9 +3,12 @@ require "dbconnection.php";
 $dbcon = createDbConnection();
 
 
-//$sql = "CREATE TABLE product(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL, PRIMARY KEY(id))";
+//$sql = "CREATE TABLE Kastike(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL, textDescription VARCHAR(4000) PRIMARY KEY(id))";
 
-$sql = "INSERT INTO product (name) VALUES ('Tuutti'),('Strösseli'),('Jaatelokone'),('Kastike')";
+//$sql = "INSERT INTO Tuutti (name) VALUES ('Suklaa'),('Mansikka'),('Kinuski'),('Vanilja')";
+
+//$sql = "INSERT INTO Tuutti VALUES
+('SKL_1','Suussa sulava suklaa','Mansikka' 100, 0.25, 24) ";
 
 $dbcon ->exec($sql);
 
